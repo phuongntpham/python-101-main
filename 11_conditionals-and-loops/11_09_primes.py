6facd0593f4ca42b4d04a32aa826c918716577ce
phuongntpham <phuongntpham@yahoo.com>
2024-10-01 21:17:41 -0700

    Updated solution

--- 11_conditionals-and-loops/11_09_primes.py
+++ 11_conditionals-and-loops/11_09_primes.py
@@ -1,1 +1,11 @@
 # Print out every prime number between 1 and 1000.
+for number in range(1, 1001):
+    if number < 2:
+        pass
+    else:
+        for i in range(2, int(number ** 0.5) + 1):
+            if number % i == 0:
+                is_prime = False
+                break
+        if is_prime:
+            print(number)
--- 11_conditionals-and-loops/11_11_fizzbuzz.py
+++ 11_conditionals-and-loops/11_11_fizzbuzz.py
@@ -4,3 +4,14 @@
 # - If the number is divisible by 3, write `Fizz` instead of the number
 # - If the number is divisible by 5, write `Buzz` instead of the number
 # - If the number is divisible by both 3 and 5, write `FizzBuzz` instead of the number
+
+n = 100
+for number in range (1, n+1):
+    if number % 3 == 0 and number % 5 == 0:
+        print('FizzBuzz')
+    elif number % 3 == 0:
+        print('Fizz')
+    elif number % 5 == 0:
+        print('Buzz')
+    else:
+        print(number)
